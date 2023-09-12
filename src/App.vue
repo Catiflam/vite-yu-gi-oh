ad6592159f478d9b6b7cf5c1b2d83c97cd0a1f86
Catiflam <gilfrid1@gmail.com>
2023-09-12 14:04:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+	data() {
+		return {};
+	},
+	components: { AppHeader },
+
+	methods: {
+		fetchCard() {},
+	},
+};
+</script>
+
+<template>
+	<AppHeader />
+</template>
+
+<style lang="scss">
+@use "./assets/scss/style.scss";
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App.vue";
+
+/* import the fontawesome */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+library.add(faUserSecret);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
